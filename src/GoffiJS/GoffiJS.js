a64e4705b5619ef54ee14f46df2cfdde4a3d5f2c
pedroGoffi <pedrohgdepauloc@gmail.com>
2022-05-26 19:37:30 -0300

    Impl of templates | g-config template > src/goffiJS/GoffiJS.js

--- src/GoffiJS/GoffiJS.js
+++ src/GoffiJS/GoffiJS.js
@@ -1,13 +1,32 @@
-class teste extends HTMLElement{
+class config extends HTMLElement{
     connectedCallback(){
-        this.innerHTML = `
-            This will be generated in a g-teste tag
-        `;
+        for(const attr of this.attributes){
+            switch(attr.name){
+                case ("_js"):
+                    this.innerHTML += `<script src="${attr.nodeValue}"></script>`;
+                    break;
+                case("_title"):
+                    this.innerHTML += `<title> ${attr.nodeValue}</title>`;
+                    break;
+                case("_css"):
+                    this.innerHTML += `<link rel="stylesheet" href="${attr.nodeValue}"/>`;
+                    break;
+                case("_name"):
+                    this.innerHTML += `<meta name="${attr.nodeValue}"/>`;
+                    break;
+                case("_charset"):
+                    this.innerHTML += `<meta charset="${attr.nodeValue}"/>`;
+                    break;
+                case("_content"):
+                    this.innerHTML += `<meta content="${attr.nodeValue}"/>`;
+                    break;
+            }
+        }
     }
 };
 (async () => {
     const TEMPLATES = [
-        teste
+        config
     ];
     TEMPLATES.forEach( tmp => {
         customElements.define(`g-${tmp.prototype.constructor.name}`, tmp);
